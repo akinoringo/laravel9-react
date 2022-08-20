3626d719e1ee003606cfeb26e7490f4e598715b1
akinoringo <wbraver@outlook.jp>
2022-08-20 21:17:32 +0900

    react-router-domの導入およびルーティングの設定

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -12,10 +12,19 @@ require('./bootstrap');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
- import React from 'react';
- import ReactDOM from 'react-dom';
- import Example from './components/Example';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from "react-router-dom";
+import About from "./About";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Route path="/about" component={About} />
+    </BrowserRouter>
+  );
+};
 
  if (document.getElementById('app')) {
-  ReactDOM.render(<Example />, document.getElementById('app'));
+  ReactDOM.render(<App />, document.getElementById('app'));
 }
