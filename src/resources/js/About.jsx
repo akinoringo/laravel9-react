28e4df5ac018710a58b4968d4909b9ed412876cf
akinoringo <wbraver@outlook.jp>
2022-08-20 21:31:43 +0900

    feat: material/uiの導入

--- src/resources/js/About.jsx
+++ src/resources/js/About.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from '@material-ui/core';
 
 const About = () => {
   return (
-    <div>
-      About
+    <div className="container">
+        <div className="row justify-content-center">
+            <div className="col-md-8">
+                <div className="card">
+                    <div className="card-header">React導入</div>
+                    <div className="card-body">Im an example component!</div>
+                    <Button color="primary" variant="contained">Material Button</Button>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
